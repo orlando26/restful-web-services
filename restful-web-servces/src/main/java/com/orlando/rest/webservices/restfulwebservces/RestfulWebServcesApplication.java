837081eb7cdbed68c84b48a83a2666a732d3c9e5
orlando <orlandolozaarr@gmail.com>
2019-10-06 19:59:03 -0500

    first commit

---
+++ restful-web-servces/src/main/java/com/orlando/rest/webservices/restfulwebservces/RestfulWebServcesApplication.java
@@ -0,0 +1,13 @@
+package com.orlando.rest.webservices.restfulwebservces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServcesApplication.class, args);
+	}
+
+}
---
+++ restful-web-servces/src/test/java/com/orlando/rest/webservices/restfulwebservces/RestfulWebServcesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.orlando.rest.webservices.restfulwebservces;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulWebServcesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
